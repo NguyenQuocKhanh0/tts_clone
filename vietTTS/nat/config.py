67d57cad60a6f2fde2a58c890092a9c0e78d2213
NguyenQuocKhanh0 <khanh200175@gmail.com>
2023-08-18 23:12:28 +0700

    up

--- vietTTS/nat/config.py
+++ vietTTS/nat/config.py
@@ -56,7 +56,7 @@ class FLAGS(Namespace):
 
     # ckpt
     ckpt_dir = Path("assets/infore/nat")
-    data_dir = Path("/kaggle/input/infore-denoise")
+    data_dir = Path("/kaggle/input/infore/InfoRe")
 
 
 class DurationInput(NamedTuple):
